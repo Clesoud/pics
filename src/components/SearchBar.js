59668a0a8440b4c32fcd8ae455b8363700e63d61
Chris Barclay <christopherjbarclay@gmail.com>
2020-01-14 00:20:57 -0800

    Near End of Section 7. SearchBar.js is broken on line 9 - console.log(this.state.term)

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,69 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = { term: '' };
+
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    console.log(this.state.term);
+  }
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input 
+              type="text" 
+              value={this.state.term} 
+              onChange={e => this.setState({ term: e.target.value })} 
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+
+export default SearchBar;
+
+/* 
+--NEW--
+
+import React from 'react';
+
+class SearchBar extends React.Component {
+  state = { term: '' };
+
+  render() {
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input 
+              type="text" 
+              value={this.state.term} 
+              onChange={e => this.setState({ term: e.target.value })} 
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
+
+
+--OLD--
+
+import React from 'react';
+
+class SearchBar extends React.Component {
   onInputChange(event) {
     console.log(event.target.value);
   }
@@ -20,3 +83,4 @@ class SearchBar extends React.Component {
 }
 
 export default SearchBar;
+*/
